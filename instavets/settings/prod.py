69ec6e10cb16a0ae20d532f5a3e9e3f5a5344cf0
Raúl Berganza <brgraul@gmail.com>
2016-07-05 19:20:56 +0200

    set db

--- instavets/settings/prod.py
+++ instavets/settings/prod.py
@@ -95,11 +95,16 @@ WSGI_APPLICATION = 'instavets.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+           'NAME': 'instavets',                      # Or path to database file if using sqlite3.
+           # The following settings are not used with sqlite3:
+           'USER': 'django_prod',
+           'PASSWORD': 'OnsTuiwlwf27kx1OT894HJMS7TMm5M',
+           'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+           'PORT': '',                      # Set to empty string for default.
+       }
+   }
 
 
 # Password validation
