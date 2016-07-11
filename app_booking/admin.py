877b6f0cd66d396b833481246bd59c4f59a7a744
bnedelcu <bogdanned32@gmail.com>
2016-07-11 09:31:29 +0200

    Name Changed Booking App

--- app_booking/admin.py
+++ app_booking/admin.py
@@ -6,7 +6,19 @@ from .models import Pet, Booking, Order, Customer
 
 #admin.site.register(UserVeterian)
 admin.site.register(Pet)
-admin.site.register(Booking)
 admin.site.register(Order)
-admin.site.register(Customer)
 #admin.site.register(UserCustomer)
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'city', 'adress', 'phone_number')
+    list_display_links = ('first_name', 'last_name')
+    list_filter = ('first_name', 'last_name', 'id')
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('date_booking', 'adress', 'city', 'customer')
+    list_filter = 'id'
+    readonly_fields = 'city'
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Booking, BookingAdmin)
