9d17a9be01cdd74f9cf2a63d43415161116aa8aa
Raúl Berganza <brgraul@gmail.com>
2016-07-12 11:35:45 +0200

    fix

--- app_booking/views.py
+++ app_booking/views.py
@@ -8,7 +8,7 @@ from datetime import datetime
 #Payment Imports
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from django.template.loader import render_to_string
+from django.template.loader import render_to_string, get_template
 from django.core.mail import EmailMessage
 from django.conf import settings
 from django.core.urlresolvers import reverse
