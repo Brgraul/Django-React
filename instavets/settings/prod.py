fe319219b829c82a33d73eae21bb2edb45cbe973
Raúl Berganza <brgraul@gmail.com>
2016-07-18 12:16:44 +0200

    media folder update

--- instavets/settings/prod.py
+++ instavets/settings/prod.py
@@ -159,7 +159,7 @@ STATICFILES_DIRS = [
     os.path.join(os.path.dirname(BASE_DIR), "static_files"),
 ]
 
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"media_files")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_collected", "media_folder")
 
 MEDIA_URL = '/media/'
 
